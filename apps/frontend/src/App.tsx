52955494086e1d51164ab087887bce761eb0b58e
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-05-05 20:14:00 +0530

    firebase

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth } from 'firebase/auth'; // Importing getAuth from firebase/auth
 
 function App() {
-  const [count, setCount] = useState(0)
+  const loginWithGoogle = async () => {
+    console.log("Authenticating user")
+
+    const provider = new GoogleAuthProvider();
+
+    try {
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user;
+      console.log(user);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+        <div className="px-6 py-4">
+            <div className="flex justify-center mx-auto">
+                {/* <img className="w-auto h-7 sm:h-8" src="https://merakiui.com/images/logo.svg" alt=""> */}
+            </div>
+
+            <h3 className="mt-3 text-xl font-medium text-center text-gray-600 dark:text-gray-200">Welcome Back</h3>
+
+            <p className="mt-1 text-center text-gray-500 dark:text-gray-400">Login or create account</p>
+
+            <form>
+                <div className="w-full mt-4">
+                    <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="email" placeholder="Email Address" aria-label="Email Address" />
+                </div>
+
+                <div className="w-full mt-4">
+                    <input className="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-500 bg-white border rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300" type="password" placeholder="Password" aria-label="Password" />
+                </div>
+
+                <div className="flex items-center justify-between mt-4">
+                    <a href="#" className="text-sm text-gray-600 dark:text-gray-200 hover:text-gray-500">Forget Password?</a>
+
+                    <button className="mx-2 px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                        Sign In
+                    </button>
+                    <button 
+                      className="px-6 py-2 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-lg hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                      onClick={loginWithGoogle}
+                    >
+                        Login With Google
+                    </button>
+                </div>
+            </form>
+        </div>
+
+        <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+            <span className="text-sm text-gray-600 dark:text-gray-200">Don't have an account? </span>
+
+            <a href="#" className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline">Register</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
-export default App
---
+++ apps/frontend/src/config/firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz-8yAG88K2j1t5TG-gos-TggVnxoUYsk",
+  authDomain: "ludofam-e8532.firebaseapp.com",
+  projectId: "ludofam-e8532",
+  storageBucket: "ludofam-e8532.appspot.com",
+  messagingSenderId: "313564043207",
+  appId: "1:313564043207:web:27c04b0eb72bc9553f2be2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
