cf53542f0f7b9184e888d84627068f816cc40eb8
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-05-16 20:01:47 +0530

    test sokcet

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -6,24 +6,28 @@ import { GameManager } from "./GameManager";
 // console.log(name);
 // // error still there
 
-const app = express();
+// const app = express();
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "hello"
-    })
-})
+// app.get("/", (req, res) => {
+//     res.json({
+//         message: "hello"
+//     })
+// })
 
-const httpServer = app.listen(3000);
+// const httpServer = app.listen(3000);
 
-const wss = new WebSocketServer({ server: httpServer });
+// const wss = new WebSocketServer({ server: httpServer });
+const wss = new WebSocketServer({ port: 8080 });
 
 const games = new GameManager();
 
 wss.on('connection', function connection(socket) {
+    console.log("in wss");
     games.handleMessages(socket);
     
-    socket.on('disconnect', games.removeUser(socket));
+    socket.on('disconnect', () => games.removeUser(socket));
 
     socket.on('error', console.error);
+
+    socket.send("hello");
 });
